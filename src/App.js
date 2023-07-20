693364861204759de9c2cde6897a51dc86597593
daniel-mawioo <mawioodaniel43@gmail.com>
2023-07-21 00:12:19 +0300

    Hero section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import logo from './assets/logo.svg';
 import './App.css';
 import './landing_page/ComingSoonComponent'
-import ComingSoonComponent from "./landing_page/ComingSoonComponent";
-import MobileScreenshotsComponent from "./landing_page/MobileScreenshotsComponent";
+// import ComingSoonComponent from "./landing_page/ComingSoonComponent";
+// import MobileScreenshotsComponent from "./landing_page/MobileScreenshotsComponent";
+import Home from './components/Home';
 
 
 
@@ -14,8 +15,9 @@ function App() {
       {/*<header className="App-header">*/}
       {/*   */}
       {/*</header>*/}
-      <ComingSoonComponent/>
-      <MobileScreenshotsComponent/>
+      {/* <ComingSoonComponent/>
+      <MobileScreenshotsComponent/> */}
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+// import LogoImg from '../assets/logo_plain.png'
+import screenshots from '../assets/screenshots.svg'
+import './Home.css'
+
+const Home = () => {
+  const [email, setEmail] = useState('');
+
+  const handleInputChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const sendEmail = () => {
+    fetch('/api/send-email', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email: 'test@gmail.com.com' }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data); // Handle the response from the server as needed
+        alert('Email sent successfully!');
+      })
+      .catch((error) => {
+        console.error('Error sending email:', error);
+        alert('Failed to send email.');
+      });
+  };
+
+  return (
+    <div className='hero-section' >
+      {/* Left side */}
+      <div className='hero-section-left'>
+        <h1>Coming Soon</h1>
+        <p>Get notified when we launch on web.</p>
+        <div className='hero-email'>
+          <input type='email' value={email} onChange={handleInputChange} placeholder='Enter your email' />
+          <button onClick={sendEmail}>Notify me</button>
+        </div>
+      </div>
+
+      {/* Right side */}
+      <div className='hero-section-right'>
+        <img src={screenshots} alt="Screenshots"></img>
+        <p>Available on google play store</p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
