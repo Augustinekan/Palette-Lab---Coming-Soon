a44d74021aa19761b64c9023e96f04ecd6c1820e
daniel-mawioo <mawioodaniel43@gmail.com>
2023-07-21 13:36:28 +0300

    Drop unused files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
-import logo from './assets/logo.svg';
 import './App.css';
-import './landing_page/ComingSoonComponent'
-// import ComingSoonComponent from "./landing_page/ComingSoonComponent";
-// import MobileScreenshotsComponent from "./landing_page/MobileScreenshotsComponent";
 import Home from './components/Home';
 import Logo from './components/Logo';
 
@@ -11,13 +7,7 @@ import Logo from './components/Logo';
 
 function App() {
   return (
-    <div
-        className="App">
-      {/*<header className="App-header">*/}
-      {/*   */}
-      {/*</header>*/}
-      {/* <ComingSoonComponent/>
-      <MobileScreenshotsComponent/> */}
+    <div className="App">
       <Logo />
       <Home />
     </div>
